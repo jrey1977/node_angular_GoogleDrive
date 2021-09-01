9d9d5e79042839116965d5ad8d65f0adb6eba9b3
Javier <jrey1977@yahoo.es>
2021-09-01 23:22:15 +0200

    Primer commit

---
+++ backup/app.js
@@ -0,0 +1,77 @@
+// Variables de entorno
+require('dotenv').config()
+
+// Importo librerías
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+
+// Google API
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+// LiveReload para recargar servidor y página al hacer cambios
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+// Arranco el server
+var app = express();
+
+// Aplico middleware de LiveReload
+app.use(connectLiveReload());
+
+// Establezco directorio y motor para plantillas 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas principales del back
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const fotosRouter = require('./routes/fotos');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/fotos', fotosRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Mando credenciales de Google
+async function signInGoogle() {
+    // Obtain user credentials to use for the request
+    const auth = await authenticate({
+      keyfilePath: path.join(__dirname, './oauth2.keys.json'),
+      scopes: 'https://www.googleapis.com/auth/drive.metadata.readonly',
+    });
+    google.options({auth});
+}
+
+signInGoogle();
+
+module.exports = app;
