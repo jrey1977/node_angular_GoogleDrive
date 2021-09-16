7d285679c746f3643e45bcaed945098103287124
Javier <jrey1977@yahoo.es>
2021-09-17 01:06:24 +0200

    Commit 17/09/2021

--- backup/controllers/fotos.js
+++ backup/controllers/fotos.js
@@ -2,12 +2,68 @@ const {google} = require('googleapis');
 const Foto = require('../models/foto');
 const Etiqueta = require('../models/etiqueta');
 
-var arrayFilteredFiles = [];
+
 const drive = google.drive('v3');
 var pageToken = null;
-var fotos;
+let arrayParentsIds = [];
+
+
+async function insertCategoria(idParent) {
+    var idParentSaved = idParent[0];
+
+    console.log('idParentSaved', idParentSaved);
+
+    /*const response2 = await drive.files.list({
+        q: "fileId contains '1qfLV68mzQZtmlVf37sFFd_Z_BDgeWBhn'",
+        fields: 'files',
+        pageSize: 1
+    });*/
+
+
+    // COSAS DE INTERNET
+    const response2 = await drive.files.get({
+        fileId: idParentSaved,
+        mymeType: 'application/vnd.google-apps.folder',
+        fields: 'name'
+    })
+
+    var nombreCategoria = response2.data.name;
+
+    var categoria = new Etiqueta ({
+        "id": idParentSaved,
+        "name": nombreCategoria,
+        "categoria": "si"
+    });
+
+    categoria.save();
+    
+}
+
+const insertMasivoCategorias = async(req, res) => {
+
+    try {
+
+        // ITERO POR EL ARRAY FORMADO POR IDS DE PARENTS
+        await arrayParentsIds.forEach( function(parentIterado) {
+            console.log('parentId iterado:', parentIterado);
+            insertCategoria(parentIterado);
+        });
+
+        console.log('Categorías insertadas');
+
+
+    } catch {
+        console.log('Error al insertar las categorías');
+    }
+
+    
+}
 
 const getFotos = async(req, res ) => {
+
+    // TODO: CUANDO ESTÉN GRABADAS LAS FOTOS Y LAS ETIQUETAS/CATEGORÍAS,
+    // HACER QUERY EN BD Y MOSTRAR PRIMERO FOTOS SIN ETIQUETAS CON CATEGORÍA "SI"
+    // Y LUEGO EL RESTO DE FOTOS
 
     // console.log('arrayFilteredFiles:', arrayFilteredFiles.length);
 
@@ -46,77 +102,63 @@ const getFotos = async(req, res ) => {
     // }
 }
 
-
-
-const insertarFotos = async(req, res ) => {
+const insertMasivoFotos = async(req, res ) => {  
 
     try{
 
-        // res.json({
-        //     resultado: 'Foto insertada'
-        // });
-
-        
-
         const response = await drive.files.list({
-            q: "mimeType='application/vnd.google-apps.folder' and '0B5pqU4vxIuqcU1BDaVdIcHRFQVk' in parents",
+            q: "mimeType contains 'image'",
             fields: 'nextPageToken, files',
             pageToken: pageToken,
-            pageSize: 1000
+            pageSize: 1
         });
     
         response.data.files.forEach(function (file) {
 
-            // if(!file.fileExtension){
-            //     file.fileExtension = "jpg";
-            // }
+                if(!file.fileExtension){
+                    file.fileExtension = "jpg";
+                }
 
-            // const foto = new Foto ({
-            //     "createdTime":file.createdTime,
-            //     "fileExtension":file.fileExtension,
-            //     "idFoto":file.id,
-            //     "width":file.imageMediaMetadata.width,
-            //     "height":file.imageMediaMetadata.height,
-            //     "modifiedTime":file.modifiedTime,
-            //     "name":file.name,
-            //     "parents": file.parents,
-            //     "size":file.size,
-            //     "webContentLink":file.webContentLink,
-            //     "webViewLink":file.webViewLink
-            // });
+                const foto = new Foto ({
+                    "createdTime":file.createdTime,
+                    "fileExtension":file.fileExtension,
+                    "idFoto":file.id,
+                    "width":file.imageMediaMetadata.width,
+                    "height":file.imageMediaMetadata.height,
+                    "modifiedTime":file.modifiedTime,
+                    "name":file.name,
+                    "parents": file.parents,
+                    "size":file.size,
+                    "webContentLink":file.webContentLink,
+                    "webViewLink":file.webViewLink
+                });
 
-            const etiqueta = new Etiqueta ({
-                "id": file.id,
-                "name": file.name,
-                "categoria": "si"
-            }) 
-            
-            etiqueta.save();
+                arrayParentsIds.push(foto.parents);
+                
+                foto.save();
+
         });
     
         pageToken = response.data.nextPageToken;
+        console.log('pageToken',pageToken)
+        console.log('arrayParentsIds', arrayParentsIds.length)
     
-        if(pageToken !== undefined){
+        if( (pageToken !== undefined) && arrayParentsIds.length < 6){
             console.log('Inserto fotos iteradas');
-            insertarFotos();
+            insertMasivoFotos();
         }else{
-            // fotos.json({
-            //     ok: true,
-            //     files: arrayFilteredFiles
-            // });
-            // arrayFilteredFiles.length = 0;
             console.log('Fotos insertadas');
+            insertMasivoCategorias();
         }
     } catch {
         res.json({
             resultado: 'Error al insertar las fotos'
         });
     }
-
-
 };
 
 module.exports = {
     getFotos,
-    insertarFotos
+    insertMasivoFotos,
+    insertMasivoCategorias
 }
--- backup/routes/fotos.js
+++ backup/routes/fotos.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const { getFotos, insertarFotos } = require('../controllers/fotos');
+const { getFotos, insertMasivoFotos } = require('../controllers/fotos');
 
 /* GET home page. */
 router.get('/', getFotos);
 
 /* Insert masivo. */
-router.get('/insertar', insertarFotos);
+router.get('/insertar', insertMasivoFotos);
 
 module.exports = router;
