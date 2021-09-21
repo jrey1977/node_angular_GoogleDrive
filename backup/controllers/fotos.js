d8b9b8425d40c21b9b057042591c2daea9c1fdf1
Javier Rey <jrey@festina.com>
2021-09-21 17:32:21 +0200

    22/09/21 0:44h

--- backup/controllers/fotos.js
+++ backup/controllers/fotos.js
@@ -39,15 +39,69 @@ const insertMasivoCategorias = async(req, res) => {
 
     try{
 
+        textoQuery = "";
+
+        arrayIds = ["0B5pqU4vxIuqcdmw5dTJLdGkwY00",
+        "8Z11kvB5jZe3qYzD16GSdwZEVfjej5k",
+        "1FKxdg5My7zpXpYrWJG1bcx8FXa6pt7RP",
+        "1xWsBPioMM2cL7hmfoll0ywEaJmuw1I",
+        "1--2zAvYRZtI-vrVsAS_5gojxauNOaqhV",
+        "1HirD4hCFpuRuGMjQZygIdptSzTCgFUZT",
+        "1MUBul860Qbvm22OoJb1AVMMTl2ubJtbC",
+        "1qfLV68mzQZtmlVf37sFFd_Z_BDgeWBhn",
+        "13eTLU37AmwEVkk3UJW6nLGEtoOKwFFZt",
+        "1tEY6D4YJGUb08hTS1oYmNTFxJSbALf1y",
+        "1CTfJxFrc4I4H-lza4P_Oic48tfUIbFOY",
+        "1ysStZqHhJY-3PyZ9QoIgpubKvn69bLDW",
+        "1eSbaZUhfvncRe0T0RBuo2XdArQE1tuXq",
+        "1GySa5QYIdFl9KX5CNXQvvi86Yf_QrzyB",
+        "1jjy2MVLco4Enf-pOCGY-x583ZoTmEo36",
+        "1v_E1jMfT1R1OUhWMcrHO95wPph9k27Ab",
+        "11c1JZ0FQ424F_1eoWS88TEpoF4jKwXbP",
+        "1u08tAlslTT7KYUR4lkg1q511rCWgt8hv",
+        "1pVd-GjERr2q3xfmCoT1yJ4tXQuJIywzz",
+        "1nRVg72Tl63crC_bRYcYtHYOQuc0QbNkP",
+        "0B5pqU4vxIuqcRXBBcVp2QVdMdzA",
+        "0B5pqU4vxIuqcZGlybzgtdUh0aDg",
+        "0B5pqU4vxIuqcYlN3VzZiSGxkZ00",
+        "0B5pqU4vxIuqcLXZMaHh0VWNnVlE",
+        "0B5pqU4vxIuqcTUZIRk43cTRlNWc",
+        "0B5pqU4vxIuqcUllxZ0tMcl9GcUk",
+        "0B5pqU4vxIuqcN09PdTkzZENPTkU",
+        "0B5pqU4vxIuqcdGZmODdrM0F0RDQ",
+        "0B5pqU4vxIuqceERxVFVCVlBKdEU",
+        "11FHLMs8Y19oRxvU0AASF2KiAlo67WMRdcTbwXn5ua6M",
+        "0B5pqU4vxIuqcc1NnM2lPNkhWWmc",
+        "0B5pqU4vxIuqcd3ZHMGhRWUVpZGs ",
+        "0B5pqU4vxIuqcZVl3NWt1MUNIalU",
+        "0B5pqU4vxIuqcWVVqYVNESXl0S3c",
+        "0B5pqU4vxIuqcMnhocUU5ejhfOHc",
+        "0B5pqU4vxIuqcNEJvY0dIN0M1NHc",
+        "0B5pqU4vxIuqcNHBmeWE1QjBubmM"]
+
+        arrayIds.forEach( (idIterada, index)=>{
+            if( index < (arrayIds.length-1) ){
+                textoQuery += `'${idIterada}' in parents or `
+            }else{
+                textoQuery += `'${idIterada}' in parents`
+            }
+        });
+
+        console.log('textoQuery:', textoQuery);
+
+        textoQuery = "'0B5pqU4vxIuqcdmw5dTJLdGkwY00' in parents or '1FKxdg5My7zpXpYrWJG1bcx8FXa6pt7RP' in parents or '1xWsBPioMM2cL7hmfoll0ywEaJmuw1I'";
+
+        
         const response2 = await drive.files.list({
-            q: "mimeType contains 'folder'",
+            q: `mimeType contains 'folder' and (${textoQuery})`,
             fields: 'nextPageToken, files',
             pageToken: pageToken,
             pageSize: 999,
             orderBy: 'createdTime desc'
         });
+        
 
-
+        
         response2.data.files.forEach(function (file) {
 
 
