e83f37fb4416e8dee36aef5af43f938b85c7c961
Javier <jrey1977@yahoo.es>
2021-11-03 23:07:58 +0100

    Infinite scroll horizontal ya funciona

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { ArchivosComponent } from './archivos/archivos.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { NgxMasonryModule } from 'ngx-masonry';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 @NgModule({
   declarations: [ArchivosComponent, NotFoundComponent, HomeComponent],
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     InfiniteScrollModule,
     NgxMasonryModule,
     BrowserAnimationsModule,
+    DragScrollModule
   ],
 })
 export class PagesModule {}
