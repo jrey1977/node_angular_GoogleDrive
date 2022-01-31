ce17ae62f46bda883691f416166f378ffe620f3c
Javier <jrey1977@yahoo.es>
2022-02-01 00:06:41 +0100

    Pongo contador a la notificación

--- frontend/src/app/utils/notification/notification.component.ts
+++ frontend/src/app/utils/notification/notification.component.ts
@@ -9,11 +9,17 @@ import { NotificationService } from './notification.service';
 })
 export class NotificationComponent implements OnInit {
   notification!: Notification;
+  counter!: boolean;
 
   constructor(public notificationService: NotificationService) {}
 
   ngOnInit(): void {
     this.notificationService.getMessage$().subscribe((notificationReceived) => {
+      console.log('cambio...');
+      this.counter = true;
+      setTimeout(() => {
+        this.counter = false;
+      }, 1200);
       this.notification = notificationReceived;
     });
   }
